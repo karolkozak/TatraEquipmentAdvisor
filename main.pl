39a1577b27fbbc6b054a0bad2e2b86cc80d1a199
Jakub Piekarz <jpiekarz12@outlook.com>
2018-11-26 09:36:03 +0100

    Change prolog code to work with one function

--- main.pl
+++ main.pl
@@ -1,63 +1,53 @@
 :- dynamic
-    xfakty/2.
+    xfakty/2,
+    xtemperatura/2.
 
-zabierz(kurtka_zimowa) :- (fakty(jest, zima);
-                        fakty(bedzie_temp, lodowato)),
-                        fakty(bedzie, padac_snieg),
-                        fakty(idzie, w_wysokie_gory).
+zabierz(kurtka_zimowa) :- (fakty(pora_roku, zima); temperatura(lodowato)),
+                        fakty(opady, snieg),
+                        fakty(cel_wyprawy, wysokie_gory).
                         
-zabierz(kurtka_przeciwdeszczowa) :- (fakty(bedzie, mgla);
-                        fakty(bedzie, deszczowo)).
+zabierz(kurtka_przeciwdeszczowa) :- (fakty(opady, mgla); fakty(opady, deszcz)).
                         
-zabierz(sniegowce) :- (fakty(jest, zima);
-                      fakty(bedzie_temp, lodowato)),
-                        fakty(bedzie, padac_snieg).
+zabierz(sniegowce) :- (fakty(pora_roku, zima); temperatura(lodowato)),
+                        fakty(opady, snieg).
                         
-zabierz(trampki) :- fakty(jest, lato),
-                        fakty(bedzie, slonecznie).
+zabierz(trampki) :- fakty(pora_roku, lato),
+                        fakty(opady, brak),
+                        fakty(cel_wyprawy, dolina).
                         
-zabierz(latarka) :- (fakty(jest, lato),
+zabierz(latarka) :- (fakty(pora_roku, lato),
                         fakty(bedzie_szedl, cztery),
                         fakty(wyruszy, po_15));
-                        (fakty(jest, zima),
+                        (fakty(pora_roku, zima),
                         fakty(bedzie_szedl, szesc),
                         fakty(wyruszy, po_10)).
 
-fakty(X,Y) :- xfakty(X,Y), !.
+fakty(X,Y) :- xfakty(X,Y).
 
-fakty(bedzie_temp, Y) :- pytaj_temperatura(Y), !.
-
-fakty(X,Y) :- pytaj(X,Y).
-
-pytaj_temperatura(Y) :- !, format('Jaka bedzie temperatura? (tylko liczby calkowite od -100 do 100)~n'),
-                    read(Reply),
-                    ((between(30,100,Reply) ->  pamietaj(bedzie_temp, goraco));
-                    (between(20,30,Reply) ->  pamietaj(bedzie_temp, cieplo));
-                    (between(10,20,Reply) ->  pamietaj(bedzie_temp, chlodno));
-                    (between(0,10,Reply) ->  pamietaj(bedzie_temp, zimno));
-                    (between(-10,0,Reply) ->  pamietaj(bedzie_temp, bardzo_zimno));
-                    (between(-100,-10,Reply) ->  pamietaj(bedzie_temp, lodowato))),
-                    pamietaj(bedzie,Y).
-    
-
-pytaj(X,Y) :- !, format('~w ~w ? (t/n)~n',[X,Y]),
-                    read(Reply),
-                    (Reply = 't'),
-                    pamietaj(X,Y).
+temperatura(X) :- xtemperatura(_, X).
                     
 pamietaj(X,Y) :- assertz(xfakty(X,Y)).
+pamietaj_temperature(X) :- ((between(30,100,X) ->  assertz(xtemperatura(X, goraco)));
+                    (between(20,30,X) ->  assertz(xtemperatura(X, cieplo)));
+                    (between(10,20,X) ->  assertz(xtemperatura(X, chlodno)));
+                    (between(0,10,X) ->  assertz(xtemperatura(X, zimno)));
+                    (between(-10,0,X) ->  assertz(xtemperatura(X, bardzo_zimno)));
+                    (between(-100,-10,X) ->  assertz(xtemperatura(X, lodowato)))).
 
-wyczysc_fakty :- write('Przycisnij cos aby wyjsc'), nl,
-                    retractall(xfakty(_,_)),
-                    read(_).
+wyczysc_pamiec :- retractall(xfakty(_,_)),
+    retractall(xtemperatura(_,_)),
+    read(_).
 
 drukuj([]).
 drukuj( [X|Y] ):- write('- '), write(X), writeln(','), drukuj(Y).
-                    
-wykonaj :- 
-    setof(X, zabierz(X), Z),    
-	writeln('Zabierz:'), drukuj(Z), nl, nl,
-    wyczysc_fakty.
-            
-wykonaj :- write('Nie jestem w stanie odgadnac co to masz zabrac w gory.'), nl,
-            wyczysc_fakty.
+
+co_zabrac(A, B, C, D) :- 
+    pamietaj_temperature(A),
+    pamietaj(pora_roku, B),
+    pamietaj(opady, C),
+    setof(X, zabierz(X), Z),
+	writeln('Zabierz:'), drukuj(Z),
+    wyczysc_pamiec.
+
+co_zabrac(_, _, _) :- write('Nie jestem w stanie odgadnac co masz zabrac w gory.'),
+            wyczysc_pamiec.
